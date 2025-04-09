7d1658fd8c3b69cb72919be857fd69c13fe3bb01
Halyna Bondar <104671539+halynabondar@users.noreply.github.com>
2025-04-09 17:36:09 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import './App.css'
+import Navbar from "./components/Navbar.jsx";
+import Main from "./components/Main.jsx";
+import Information from "./components/Information.jsx";
+import OurProducts from "./components/OurProducts.jsx";
+import RoomsInspiration from "./components/RoomsInspiration.jsx";
+import TipsTricks from "./components/TipsTricks.jsx";
+import FuniroFurniture from "./components/FuniroFurniture.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
-
   return (
     <>
+      <Navbar />
+      <Main />
+      <Information />
+      <OurProducts />
+      <RoomsInspiration />
+      <TipsTricks />
+      <FuniroFurniture />
+      <Footer />
     </>
   )
 }
